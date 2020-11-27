a29395b5da947621775252ccc26a64ec8d0f561d
jihoBae <jiho@dgu.ac.kr>
2020-11-28 03:16:21 +0900

    20.11.28 edit code

--- pro08/src/sec05/ex03/ContextFileServlet.java
+++ pro08/src/sec05/ex03/ContextFileServlet.java
@@ -13,32 +13,31 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-@WebServlet("cfile")
+
+@WebServlet("/cfile")
 public class ContextFileServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html;charset=utf-8");
+		response.setContentType("text/html;charset=euc-kr");
 		PrintWriter out = response.getWriter();
 		ServletContext context = getServletContext();
 		InputStream is = context.getResourceAsStream("/WEB-INF/bin/init.txt");
 		BufferedReader buffer = new BufferedReader(new InputStreamReader(is));
-		
+
 		String menu = null;
 		String menu_member = null;
 		String menu_order = null;
 		String menu_goods = null;
-		while((menu=buffer.readLine()) != null){
+		while ((menu = buffer.readLine()) != null) {
 			StringTokenizer tokens = new StringTokenizer(menu, ",");
 			menu_member = tokens.nextToken();
 			menu_order = tokens.nextToken();
 			menu_goods = tokens.nextToken();
 		}
-		
 		out.print("<html><body>");
-		out.print("<table border=1 cellspacing=0><tr>메뉴 이름</tr>");
-		out.print("<tr><td>" + menu_member + "</td></tr>");
-		out.print("<tr><td>" + menu_order + "</td></tr>");
-		out.print("<tr><td>" + menu_goods + "</td></tr>");
-		out.print("</tr></table></body></html>");
+		out.print(menu_member + "<br>");
+		out.print(menu_order + "<br>");
+		out.print(menu_goods + "<br>");
+		out.print("</body></html>");
+		out.close();
 	}
-
 }
